cbc60cd0530a73ee3a49297774fb5ce606183d41
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-11-12 16:16:30 +0000

    add products on index models and index routes

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 import CategoryModel from "./category.model.js";
 import UserModel from "./user.model.js";
 import RoleModel from "./role.model.js";
+import ProductModel from "./product.model.js";
 
 export {
     CategoryModel,
     UserModel,
     RoleModel,
+    ProductModel
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import AuthRouter from "./auth.router.js";
 import CategoriesRouter from "./categories.router.js";
 import UsersRouter from "./users.router.js";
+import ProductsRouter from "./products.router.js";
 
 export {
     AuthRouter,
     CategoriesRouter,
     UsersRouter,
+    ProductsRouter
 };
